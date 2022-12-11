13ac5184848ff4a695d5af549d68b004e5bf6f9d
Vladislav <vnl.i@ya.ru>
2022-12-11 22:38:19 +0300

    Домашняя задача 01 семинар 5

--- DZS5.001/Program.cs
+++ DZS5.001/Program.cs
@@ -1,2 +1,45 @@
-﻿// // See https://aka.ms/new-console-template for more information
-// Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] GenerateArray(int Length)
+{
+    int[] array = new int [Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }   
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Promt(string messege)
+{
+    System.Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Eventcount(int[] array)
+{
+    int counter = 0;
+    foreach (int item in array)
+    {
+        if (item % 2 == 0)
+        {
+            counter++;
+        }
+    }
+    return counter;
+}
+
+int Length = Promt("Input Lenght of array");
+int[] array = GenerateArray(Length);
+PrintArray(array);
+Console.WriteLine(Eventcount(array));
