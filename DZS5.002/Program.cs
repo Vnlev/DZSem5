0f0bef9fbc7c3abb7cf00cd22b34460a741ab80a
Vladislav <vnl.i@ya.ru>
2022-12-11 22:39:28 +0300

    Домашняя задача 2 семинар 5

---
+++ DZS5.002/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int Length)
+{
+    int[] array = new int [Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }   
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int j = 0; j < array.Length; j++)
+    {
+        System.Console.Write(array[j] + "\t");
+    }
+    System.Console.WriteLine();
+}
+
+int Promt(string messege)
+{
+    System.Console.WriteLine(messege);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Eventcount(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+int Length = Promt("Введите длину массива");
+int[] array = GenerateArray(Length);
+PrintArray(array);
+Console.WriteLine(Eventcount(array));
